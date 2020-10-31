eee921a08fec1141e0294aa34b82774ee44aaf7d
unknown <bryan.vanmoer@student.vinci.be>
2020-10-31 15:44:57 +0100

    add - test getHours()

--- BerlinClock.php
+++ BerlinClock.php
@@ -3,5 +3,10 @@
 
 class BerlinClock
 {
-
+    public function getHours($randomTime) :int
+    {
+        $hours = [];
+        preg_match("/[0-9]{1,2}:[0-9]{1,2}:[0-9]{1,2}/", $randomTime, $hours);
+        return (int)$hours[0];
+    }
 }
